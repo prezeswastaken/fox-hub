dd510c395006150cfc4ca211c9a0c63abb6e8e2d
prezeswastaken <kacper.preyzner@studenci.collegiumwitelona.pl>
2023-08-16 01:12:34 +0200

    made gallery look good

--- resources/js/Pages/Show.tsx
+++ resources/js/Pages/Show.tsx
@@ -1,3 +1,6 @@
+import DangerButton from "@/Components/DangerButton";
+import PrimaryButton from "@/Components/PrimaryButton";
+import SecondaryButton from "@/Components/SecondaryButton";
 import NormalLayout from "@/Layouts/NormalLayout";
 
 type Photo = {
@@ -10,15 +13,24 @@ type Props = {
 
 export default function Show({ photos }: Props) {
     const photosObject = photos.map((photo) => (
-        <div className="w-52 h-52 rounded">
-            <img src={photo.url} />
+        <div className="flex overflow-hidden relative justify-center items-center w-80 h-80">
+            <img
+                src={photo.url}
+                className="flex object-cover flex-col justify-end w-80 h-80 rounded"
+            />
+            <div className="flex absolute bottom-0 left-0 justify-between items-center py-2 px-2 w-full bg-gray-500 bg-opacity-50">
+                <PrimaryButton>delete</PrimaryButton>
+                <PrimaryButton>save</PrimaryButton>
+            </div>
         </div>
     ));
     return (
         <>
             <NormalLayout>
-                <div className="grid grid-cols-3 grid-rows-3 gap-4">
-                    {photosObject}
+                <div className="flex justify-center">
+                    <div className="grid grid-cols-3 gap-20">
+                        {photosObject}
+                    </div>
                 </div>
             </NormalLayout>
         </>
